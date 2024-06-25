df62d8092b7ce23a8edc4e98dfee90fb3b43519e
Aleksandr Kropivnyy <kropivnyy@gmail.com>
2024-06-25 16:40:24 +0300

    added redirect

--- src/utils/go-redirect.js
+++ src/utils/go-redirect.js
@@ -1,10 +1,7 @@
-import queryString from 'query-string';
-
 export const goRedirect = () => {
   //   ua_log('force_redirect');
-  const searchString = queryString.parse(window.location.search);
 
   window.location.replace(
-    `${import.meta.env.VITE_REDIRECT_URL}?${searchString}`,
+    `${import.meta.env.VITE_REDIRECT_URL}?${window.location.search}`,
   );
 };
