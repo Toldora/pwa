0f25cde6f3904d35c59c2cbcb8ea11befdcb1ccd
Aleksandr Kropivnyy <kropivnyy@gmail.com>
2024-06-25 15:50:49 +0300

    Initial

---
+++ src/main.js
@@ -0,0 +1,8 @@
+import '@/styles/index.scss';
+
+import 'virtual:svg-icons-register';
+import useViewportSizes from '@/js/use-viewport-sizes';
+import '@/js/expand-btn';
+import '@/js/pwa';
+
+useViewportSizes();
